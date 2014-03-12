f1f5ab70eed081a0b253fe4f0aa96e277c8433d1
Andreas Messer <andi@bastelmap.de>
2014-03-12 22:27:27 +0100

    General algorithm improvements:   - add scaling to dft   - implement math class as an alias for standard math   - add missing functions to rambuffer

---
+++ src/algorithm/math.hpp
@@ -0,0 +1,53 @@
+/*
+ *  Copyright 2014 Andreas Messer <andi@bastelmap.de>
+ *
+ *  This file is part of the Embedded C++ Platform Project.
+ *
+ *  Embedded C++ Platform Project (ECPP) is free software: you can
+ *  redistribute it and/or modify it under the terms of the GNU General
+ *  Public License as published by the Free Software Foundation,
+ *  either version 3 of the License, or (at your option) any later
+ *  version.
+ *
+ *  Embedded C++ Platform Project is distributed in the hope that it
+ *  will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ *  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with ECPP.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  As a special exception, the copyright holders of ECPP give you
+ *  permission to link ECPP with independent modules to produce an
+ *  executable, regardless of the license terms of these independent
+ *  modules, and to copy and distribute the resulting executable under
+ *  terms of your choice, provided that you also meet, for each linked
+ *  independent module, the terms and conditions of the license of that
+ *  module.  An independent module is a module which is not derived from
+ *  or based on ECPP.  If you modify ECPP, you may extend this exception
+ *  to your version of ECPP, but you are not obligated to do so.  If you
+ *  do not wish to do so, delete this exception statement from your
+ *  version.
+ *  */
+
+#ifndef MATH_HPP_
+#define MATH_HPP_
+
+#include <cmath>
+
+namespace Platform {
+  namespace Algorithm {
+    class CMath {
+    public:
+      static constexpr double m_pi    = M_PI;
+      static constexpr double m_sqrt2 = 1.4142135623730951;
+
+      static constexpr double sin(double arg) { return ::std::sin(arg); }
+      static constexpr double cos(double arg) { return ::std::cos(arg); }
+    };
+  }
+};
+
+
+
+#endif /* MATH_HPP_ */
